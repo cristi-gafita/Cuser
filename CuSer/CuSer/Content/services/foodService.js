e05c20cbce03b123ab2f967c135c3214420655aa
cristi-gafita <gafita.cristi@yahoo.com>
2016-01-04 16:49:43 +0200

    added a few services

---
+++ CuSer/CuSer/Content/services/foodService.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("CuserApp")
+        .service("foodService",
+        ["syncService", foodService]);
+
+    function foodService(syncService) {
+        var vm = this;
+
+        vm.getFoodCategories = function (id, cb) {
+            syncService.getReviews(id, cb);
+        }
+
+        vm.getFoodFromCategory = function (categoryId, cb) {
+            syncService.addNewReview(categoryId, cb);
+        }
+    }
+}());
---
+++ CuSer/CuSer/Content/services/reviewsService.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("CuserApp")
+        .service("reviewsService",
+        ["syncService", reviewsService]);
+
+    function reviewsService(syncService) {
+        var vm = this;
+
+        vm.getReviews = function (id, cb) {
+            syncService.getReviews(id, cb);
+        }
+
+        vm.addNewReview = function (review, cb) {
+            syncService.addNewReview(review, cb);
+        }
+    }
+}());

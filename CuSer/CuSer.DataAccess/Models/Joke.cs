dda8d82e0790122b1879bb93ed06156f6252f885
vpatrasc <patra.vlad@gmail.com>
2016-01-05 09:35:04 +0200

    Created models in DataAccess,Implementation and Interfaces in  Logic layer.

---
+++ CuSer/CuSer.DataAccess/Models/Joke.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuSer.DataAccess.Models
+{
+    class Joke
+    {
+        public string Title { get; set; }
+
+        public string Joke { get; set; }
+    }
+}
---
+++ CuSer/CuSer.DataAccess/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuSer.DataAccess.Models
+{
+    class Product
+    {
+        public string Name { get; set; }
+
+        public List<Ingredient> Ingredients { get; set; }
+
+        public int Price { get; set; }
+
+        public List<Feedback> Feedbacks { get; set; }
+    }
+}
